ad2dd535a65e4e8e882ec04af48c967a28311188
Devic Luka <38909217+DevicLuka@users.noreply.github.com>
2021-10-15 20:11:22 +0200

    RRW-1 feature: Create Car

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public int Speed { get; set; }
+        public double MelfunctionChance { get; set; }
+        public int MelfunctionsOccured { get; set; }
+        public int DistanceCoverdInMiles { get; set; }
+        public int FinishedRace { get; set; }
+        public int RacedForHours { get; set; }
+    }
+}
