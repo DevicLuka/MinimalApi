45c6f7d61d7db26e9c1378e474e41138ebb5ef07
Devic Luka <38909217+DevicLuka@users.noreply.github.com>
2021-10-20 16:21:04 +0200

    RRW-3 feature: Add EF and configuration

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    x => x.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
