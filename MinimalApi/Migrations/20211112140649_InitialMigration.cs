f13cf45040958586e91bb7df7775ee8fee2e07a2
Devic Luka <lukadevic88@gmail.com>
2021-11-12 15:59:32 +0100

    MIN-2.1 feature: Add Cars persistance

---
+++ MinimalApi/Migrations/20211112140649_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApi.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    MelfunctionChance = table.Column<double>(type: "float", nullable: false),
+                    MelfunctionsOccured = table.Column<int>(type: "int", nullable: false),
+                    DistanceCoverdInMiles = table.Column<int>(type: "int", nullable: false),
+                    FinishedRace = table.Column<bool>(type: "bit", nullable: false),
+                    RacedForHours = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add the Entity Framework Core DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<RaceDb>(options =>
+options.UseSqlServer(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,62 +30,89 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-
 #region Cars endpoints
 
-app.MapGet("api/cars", () =>
+// Get cars
+app.MapGet("api/cars", 
+    (RaceDb db) =>
 {
-    var car1 = new Car
-    {
-        TeamName = "Team A"
-    };
-    var car2 = new Car
-    {
-        TeamName = "Team B"
-    };
-
-    var cars = new List<Car>
-    {
-        car1, car2
-    };
-
-    return cars;
+    var cars = db.Cars.ToList();
+    return Results.Ok(cars);
 })
     .WithName("GetCars")
     .WithTags("Cars");
 
+// Get car
 app.MapGet("api/cars/{id}",
-    (int id) =>
+    (int id, RaceDb db) =>
     {
-        var car1 = new Car
+        var dbCar = db.Cars.FirstOrDefault(x => x.Id == id);
+
+        if(dbCar == null)
         {
-            TeamName = "Team A"
-        };
-        return car1;
+            return Results.NotFound($"Car with id: {id} isn't found");
+        }
+
+        return Results.Ok(dbCar);
     })
     .WithName("GetCar")
     .WithTags("Cars");
 
+// Create car
 app.MapPost("api/cars",
-    (Car car) =>
+    (CarCreateModel carModel, RaceDb db) =>
     {
-        return car;
+        var newCar = new Car
+        {
+            TeamName = carModel.TeamName,
+            Speed = carModel.Speed,
+            MelfunctionChance = carModel.MelfunctionChance
+        };
+
+        db.Cars.Add(newCar);
+        db.SaveChanges();
+
+        return Results.Ok(newCar);
     })
     .WithName("CreateCar")
     .WithTags("Cars");
 
+// Update car
 app.MapPut("api/cars/{id}",
-    (Car car) =>
+    ([FromQuery] int id, [FromBody] CarCreateModel carModel, RaceDb db) =>
     {
-        return car;
+        var dbCar = db.Cars.FirstOrDefault(x => x.Id == id);
+
+        if (dbCar == null)
+        {
+            return Results.NotFound($"Car with id: {id} isn't found");
+        }
+
+        dbCar.TeamName = carModel.TeamName;
+        dbCar.Speed = carModel.Speed;
+        dbCar.MelfunctionChance = carModel.MelfunctionChance;
+        db.SaveChanges();
+
+        return Results.Ok(dbCar);
     })
     .WithName("UpdateCar")
     .WithTags("Cars");
 
+// Delete car
 app.MapDelete("api/cars/{id}",
-    (int id) =>
+    (int id, RaceDb db) =>
     {
-        return $"Car with id: {id} was succesfully deleted";
+        var dbCar = db.Cars.FirstOrDefault(x => x.Id == id);
+
+        if (dbCar == null)
+        {
+            return Results.NotFound($"Car with id: {id} isn't found");
+        }
+
+        db.Remove(dbCar);
+        db.SaveChanges();
+
+        return Results.Ok($"Car with id: {id} was succesfully deleted");
     })
     .WithName("DeleteCar")
     .WithTags("Cars");
@@ -165,7 +201,6 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-// Models
 #region Models
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
@@ -185,6 +220,13 @@ public record Car
     public int RacedForHours { get; set; }
 }
 
+public record CarCreateModel
+{
+    public string TeamName { get; set; }
+    public int Speed { get; set; }
+    public double MelfunctionChance { get; set; }
+}
+
 public record Motorbike
 {
     public int Id { get; set; }
@@ -198,3 +240,15 @@ public record Motorbike
 }
 
 #endregion
+
+
+// Persistance
+
+public class RaceDb : DbContext
+{
+    public RaceDb(DbContextOptions<RaceDb> options) : base(options)
+    {
+    }
+
+    public DbSet<Car> Cars { get; set; }
+}
